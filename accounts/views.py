eb5090d8885459080bba9b72d3f2d3c535202c66
booorayan <boorayo@yahoo.com>
2023-07-12 23:26:13 +0300

    render templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
 # Create your views here.
 def home(request):
-    return render(request, '../templates/dashboard.html')
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'customers': customers, 'orders': orders,
+               'total_orders': total_orders, 'delivered': delivered,
+               'pending': pending}
+
+    return render(request, 'dashboard.html', context)
 
 
 def products(request):
-    return render(request, '../templates/products.html')
+    productss = Product.objects.all()
+
+    context = {'products': productss}
+
+    return render(request, 'products.html', context)
 
 
-def customer(request):
-    return render(request, '../templates/customer.html')
+def customer(request, pk):
+    kastama = Customer.objects.get(id=pk)
+
+    orders = kastama.order_set.all()
+    orders_count = orders.count()
+
+    context = {'customer': kastama, 'orders': orders, 'orders_count': orders_count}
+
+    return render(request, 'customer.html', context)
